740eb6fa8ac6b33d1073f55f3065443c7b519834
Cesar Flores <cesar.flores@gmail.com>
2025-12-22 23:02:21 -0600

    Se agragó el la barra de navegación

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,1 +1,7 @@
-<template>Footer</template>
+<template>
+    <footer class="border-top footer text-muted p-2 bg-body-teryiary">
+        <div class="text-center">
+            Hecho por <i class="bi bi-r-circle"></i> Cesar Flores
+        </div>
+    </footer>
+</template>
